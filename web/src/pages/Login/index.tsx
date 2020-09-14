c169abe034aa2d67d8251ba6c855b9dd7a43ec8b
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-14 17:41:21 -0300

    web: Add global styles & Login page component

---
+++ web/src/pages/Login/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Login.css';
+
+import logo from '../../assets/logo.svg';
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="Tindev Logo"/>
+        <input
+          type="text"
+          placeholder="Digite seu username no Github"
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
