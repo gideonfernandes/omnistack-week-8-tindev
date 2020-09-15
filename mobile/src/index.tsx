a9f07d8aa5724569c62c36621c542a550bfc6a8c
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-15 18:10:29 -0300

    mobile: Add react-native init with typescript, cleanup

---
+++ mobile/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import Login from './pages/Login';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hello World from React Native!</Text>
+      <Login />
+    </>
+  );
+};
+
+export default App;
